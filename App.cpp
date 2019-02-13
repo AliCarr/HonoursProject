2304ddc2c8b78bd769842ca2f475cadc63e1d8b4
Ali <alastairrosscarr@gmail.com>
2019-02-13 12:40:50 +0000

    Return to normal
    Full revert back to working system. Randomness is anything but, and the method in which I am working this is bad at best. Will need to try condense the amount of mesh geos to one, having it be populated with all the individual particles

--- App.cpp
+++ App.cpp
@@ -1,30 +1,30 @@
 #include "App.h"
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance,
-				   PSTR cmdLine, int showCmd)
+	PSTR cmdLine, int showCmd)
 {
 	// Enable run-time memory check for debug builds.
 #if defined(DEBUG) | defined(_DEBUG)
-	_CrtSetDbgFlag( _CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF );
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 #endif
 
-    try
-    {
-        App theApp(hInstance);
-        if(!theApp.Initialize())
-            return 0;
+	try
+	{
+		App theApp(hInstance);
+		if (!theApp.Initialize())
+			return 0;
 
-        return theApp.Run();
-    }
-    catch(DxException& e)
-    {
-        MessageBox(nullptr, e.ToString().c_str(), L"HR Failed", MB_OK);
-        return 0;
-    }
+		return theApp.Run();
+	}
+	catch (DxException& e)
+	{
+		MessageBox(nullptr, e.ToString().c_str(), L"HR Failed", MB_OK);
+		return 0;
+	}
 }
 
 App::App(HINSTANCE hInstance)
-: D3DApp(hInstance) 
+	: D3DApp(hInstance)
 {
 }
 
@@ -34,29 +34,29 @@ App::~App()
 
 bool App::Initialize()
 {
-    if(!D3DApp::Initialize())
+	if (!D3DApp::Initialize())
 		return false;
-		
-    // Reset the command list to prep for initialization commands.
-    ThrowIfFailed(mCommandList->Reset(mDirectCmdListAlloc.Get(), nullptr));
- 
-    BuildDescriptorHeaps();
+
+	// Reset the command list to prep for initialization commands.
+	ThrowIfFailed(mCommandList->Reset(mDirectCmdListAlloc.Get(), nullptr));
+
+	BuildDescriptorHeaps();
 	BuildConstantBuffers();
-    BuildRootSignature();
-    BuildShadersAndInputLayout();
+	BuildRootSignature();
+	BuildShadersAndInputLayout();
 	//mScene = new Scene();
-	
+
 	//mParticle[0] = new Particle(mBoxGeo, md3dDevice, mCommandList);
 	pManager = new ParticleManager(md3dDevice, mCommandList, mBoxGeo);
-    BuildModel();
-    BuildPSO();
-    // Execute the initialization commands.
-    ThrowIfFailed(mCommandList->Close());
+	//  BuildModel();
+	BuildPSO();
+	// Execute the initialization commands.
+	ThrowIfFailed(mCommandList->Close());
 	ID3D12CommandList* cmdsLists[] = { mCommandList.Get() };
 	mCommandQueue->ExecuteCommandLists(_countof(cmdsLists), cmdsLists);
 
-    // Wait until initialization is complete.
-    FlushCommandQueue();
+	// Wait until initialization is complete.
+	FlushCommandQueue();
 
 	timer = 0;
 	mControl = new Controls();
@@ -68,9 +68,9 @@ void App::OnResize()
 {
 	D3DApp::OnResize();
 
-    // The window resized, so update the aspect ratio and recompute the projection matrix.
-    XMMATRIX P = XMMatrixPerspectiveFovLH(0.25f*MathHelper::Pi, AspectRatio(), 1.0f, 1000.0f);
-    XMStoreFloat4x4(&mProj, P);
+	// The window resized, so update the aspect ratio and recompute the projection matrix.
+	XMMATRIX P = XMMatrixPerspectiveFovLH(0.25f*MathHelper::Pi, AspectRatio(), 1.0f, 1000.0f);
+	XMStoreFloat4x4(&mProj, P);
 }
 
 void App::Update(const GameTimer& gt)
@@ -93,7 +93,8 @@ void App::Update(const GameTimer& gt)
 	XMMATRIX rotation = XMMatrixRotationRollPitchYaw(0, 0, 10);
 
 	XMMATRIX world = XMLoadFloat4x4(&mWorld);
-//	pManager->Update(world, gt.DeltaTime());
+	pManager->Update(world, gt.DeltaTime());
+
 	//world = XMMatrixMultiply(world, rotation);
 	XMMATRIX proj = XMLoadFloat4x4(&mProj);
 	XMMATRIX worldViewProj = world * view * proj;
@@ -101,10 +102,10 @@ void App::Update(const GameTimer& gt)
 	// Update the constant buffer with the latest worldViewProj matrix.
 	ObjectConstants objConstants;
 	//pManager->UpdateCBuffers(mObjectCB);
-   
 
 
-	XMStoreFloat4x4(&objConstants.WorldViewProj , XMMatrixTranspose( worldViewProj));
+
+	XMStoreFloat4x4(&objConstants.WorldViewProj, XMMatrixTranspose(worldViewProj));
 	timer += gt.DeltaTime();
 
 	objConstants.yPosiiton = timer;
@@ -112,31 +113,31 @@ void App::Update(const GameTimer& gt)
 
 
 	objConstants.pulseColour = XMFLOAT4(1, 0, 0, 1);
-    mObjectCB->CopyData(0, objConstants);
+	mObjectCB->CopyData(0, objConstants);
 }
 
 void App::Draw(const GameTimer& gt)
 {
-    // Reuse the memory associated with command recording.
-    // We can only reset when the associated command lists have finished execution on the GPU.
+	// Reuse the memory associated with command recording.
+	// We can only reset when the associated command lists have finished execution on the GPU.
 	ThrowIfFailed(mDirectCmdListAlloc->Reset());
 
 	// A command list can be reset after it has been added to the command queue via ExecuteCommandList.
-    // Reusing the command list reuses memory.
-    ThrowIfFailed(mCommandList->Reset(mDirectCmdListAlloc.Get(), mPSO.Get()));
+	// Reusing the command list reuses memory.
+	ThrowIfFailed(mCommandList->Reset(mDirectCmdListAlloc.Get(), mPSO.Get()));
 
-    mCommandList->RSSetViewports(1, &mScreenViewport);
-    mCommandList->RSSetScissorRects(1, &mScissorRect);
+	mCommandList->RSSetViewports(1, &mScreenViewport);
+	mCommandList->RSSetScissorRects(1, &mScissorRect);
 
-    // Indicate a state transition on the resource usage.
+	// Indicate a state transition on the resource usage.
 	mCommandList->ResourceBarrier(1, &CD3DX12_RESOURCE_BARRIER::Transition(CurrentBackBuffer(),
 		D3D12_RESOURCE_STATE_PRESENT, D3D12_RESOURCE_STATE_RENDER_TARGET));
 
-    // Clear the back buffer and depth buffer.
-    mCommandList->ClearRenderTargetView(CurrentBackBufferView(), Colors::Black, 0, nullptr);
-    mCommandList->ClearDepthStencilView(DepthStencilView(), D3D12_CLEAR_FLAG_DEPTH | D3D12_CLEAR_FLAG_STENCIL, 1.0f, 0, 0, nullptr);
-	
-    // Specify the buffers we are going to render to.
+	// Clear the back buffer and depth buffer.
+	mCommandList->ClearRenderTargetView(CurrentBackBufferView(), Colors::Black, 0, nullptr);
+	mCommandList->ClearDepthStencilView(DepthStencilView(), D3D12_CLEAR_FLAG_DEPTH | D3D12_CLEAR_FLAG_STENCIL, 1.0f, 0, 0, nullptr);
+
+	// Specify the buffers we are going to render to.
 	mCommandList->OMSetRenderTargets(1, &CurrentBackBufferView(), true, &DepthStencilView());
 
 	ID3D12DescriptorHeap* descriptorHeaps[] = { mCbvHeap.Get() };
@@ -144,32 +145,26 @@ void App::Draw(const GameTimer& gt)
 
 	mCommandList->SetGraphicsRootSignature(mRootSignature.Get());
 
-	mCommandList->IASetVertexBuffers(0, 1, &mBoxGeo->VertexBufferView());
-
+	/*mCommandList->IASetVertexBuffers(0, 1, &mBoxGeo->VertexBufferView());
 	mCommandList->IASetIndexBuffer(&mBoxGeo->IndexBufferView());
-    mCommandList->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
-    
-    mCommandList->SetGraphicsRootDescriptorTable(0, mCbvHeap->GetGPUDescriptorHandleForHeapStart());
+	mCommandList->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
 
-    mCommandList->DrawIndexedInstanced(mBoxGeo->DrawArgs["box"].IndexCount, 1, mBoxGeo->DrawArgs["box"].StartIndexLocation, mBoxGeo->DrawArgs["box"].BaseVertexLocation, 0);
+	mCommandList->SetGraphicsRootDescriptorTable(0, mCbvHeap->GetGPUDescriptorHandleForHeapStart());
+	mCommandList->DrawIndexedInstanced(mBoxGeo->DrawArgs["particle"].IndexCount, 1, mBoxGeo->DrawArgs["particle"].StartIndexLocation, mBoxGeo->DrawArgs["particle"].BaseVertexLocation, 0);*/
 
 
-<<<<<<< HEAD
-	pManager->Render(mCommandList, mCbvHeap, mCbvSrvUavDescriptorSize, md3dDevice, mBoxGeo);
-=======
-	//pManager->Render(mCommandList, mCbvHeap, mCbvSrvUavDescriptorSize);
->>>>>>> parent of 5b3725a... Switch in render item
-    // Indicate a state transition on the resource usage.
+	pManager->Render(mCommandList, mCbvHeap, mCbvSrvUavDescriptorSize, md3dDevice);
+	// Indicate a state transition on the resource usage.
 	mCommandList->ResourceBarrier(1, &CD3DX12_RESOURCE_BARRIER::Transition(CurrentBackBuffer(),
 		D3D12_RESOURCE_STATE_RENDER_TARGET, D3D12_RESOURCE_STATE_PRESENT));
 
-    // Done recording commands.
+	// Done recording commands.
 	ThrowIfFailed(mCommandList->Close());
- 
-    // Add the command list to the queue for execution.
+
+	// Add the command list to the queue for execution.
 	ID3D12CommandList* cmdsLists[] = { mCommandList.Get() };
 	mCommandQueue->ExecuteCommandLists(_countof(cmdsLists), cmdsLists);
-	
+
 	// swap the back and front buffers
 	ThrowIfFailed(mSwapChain->Present(0, 0));
 	mCurrBackBuffer = (mCurrBackBuffer + 1) % SwapChainBufferCount;
@@ -187,7 +182,7 @@ void App::OnMouseDown(WPARAM btnState, int x, int y)
 
 void App::OnMouseUp(WPARAM btnState, int x, int y)
 {
-	mControl->OnMouseUp( btnState,  x,  y);
+	mControl->OnMouseUp(btnState, x, y);
 }
 
 void App::OnMouseMove(WPARAM btnState, int x, int y)
@@ -197,13 +192,13 @@ void App::OnMouseMove(WPARAM btnState, int x, int y)
 
 void App::BuildDescriptorHeaps()
 {
-    D3D12_DESCRIPTOR_HEAP_DESC cbvHeapDesc;
-    cbvHeapDesc.NumDescriptors = 11;
-    cbvHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV;
-    cbvHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE;
+	D3D12_DESCRIPTOR_HEAP_DESC cbvHeapDesc;
+	cbvHeapDesc.NumDescriptors = 11;
+	cbvHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV;
+	cbvHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE;
 	cbvHeapDesc.NodeMask = 0;
-    ThrowIfFailed(md3dDevice->CreateDescriptorHeap(&cbvHeapDesc,
-        IID_PPV_ARGS(&mCbvHeap)));
+	ThrowIfFailed(md3dDevice->CreateDescriptorHeap(&cbvHeapDesc,
+		IID_PPV_ARGS(&mCbvHeap)));
 }
 
 void App::BuildConstantBuffers()
@@ -213,9 +208,9 @@ void App::BuildConstantBuffers()
 	UINT objCBByteSize = d3dUtil::CalcConstantBufferByteSize(sizeof(ObjectConstants));
 
 	D3D12_GPU_VIRTUAL_ADDRESS cbAddress = mObjectCB->Resource()->GetGPUVirtualAddress();
-    // Offset to the ith object constant buffer in the buffer.
-    int boxCBufIndex = 0;
-	cbAddress += boxCBufIndex*objCBByteSize;
+	// Offset to the ith object constant buffer in the buffer.
+	int boxCBufIndex = 0;
+	cbAddress += boxCBufIndex * objCBByteSize;
 
 	D3D12_CONSTANT_BUFFER_VIEW_DESC cbvDesc;
 	cbvDesc.BufferLocation = cbAddress;
@@ -237,7 +232,7 @@ void App::BuildRootSignature()
 	slotRootParameter[0].InitAsDescriptorTable(1, &cbvTable);
 
 	// A root signature is an array of root parameters.
-	CD3DX12_ROOT_SIGNATURE_DESC rootSigDesc(1, slotRootParameter, 0, nullptr, 
+	CD3DX12_ROOT_SIGNATURE_DESC rootSigDesc(1, slotRootParameter, 0, nullptr,
 		D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT);
 
 	// create a root signature with a single slot which points to a descriptor range consisting of a single constant buffer
@@ -246,7 +241,7 @@ void App::BuildRootSignature()
 	HRESULT hr = D3D12SerializeRootSignature(&rootSigDesc, D3D_ROOT_SIGNATURE_VERSION_1,
 		serializedRootSig.GetAddressOf(), errorBlob.GetAddressOf());
 
-	if(errorBlob != nullptr)
+	if (errorBlob != nullptr)
 	{
 		::OutputDebugStringA((char*)errorBlob->GetBufferPointer());
 	}
@@ -261,49 +256,49 @@ void App::BuildRootSignature()
 
 void App::BuildShadersAndInputLayout()
 {
-    HRESULT hr = S_OK;
-    
+	HRESULT hr = S_OK;
+
 	mvsByteCode = d3dUtil::CompileShader(L"Shaders\\colorVS.hlsl", nullptr, "VS", "vs_5_0");
 	mpsByteCode = d3dUtil::CompileShader(L"Shaders\\colorPS.hlsl", nullptr, "PS", "ps_5_0");
 
-    mInputLayout =
-    {
+	mInputLayout =
+	{
 		{ "POSITION", 0,    DXGI_FORMAT_R32G32B32_FLOAT, 0,   0, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 },
-		{ "TEX",      0,       DXGI_FORMAT_R32G32_FLOAT, 0,  12, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 },
-		{ "COLOR",    0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0,  20, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 }
-    };
+	{ "TEX",      0,       DXGI_FORMAT_R32G32_FLOAT, 0,  12, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 },
+	{ "COLOR",    0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0,  20, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 }
+	};
 
 }
 
 void App::BuildPSO()
 {
-    D3D12_GRAPHICS_PIPELINE_STATE_DESC psoDesc;
-    ZeroMemory(&psoDesc, sizeof(D3D12_GRAPHICS_PIPELINE_STATE_DESC));
-    psoDesc.InputLayout = { mInputLayout.data(), (UINT)mInputLayout.size() };
-    psoDesc.pRootSignature = mRootSignature.Get();
-    psoDesc.VS = 
-	{ 
-		reinterpret_cast<BYTE*>(mvsByteCode->GetBufferPointer()), 
-		mvsByteCode->GetBufferSize() 
+	D3D12_GRAPHICS_PIPELINE_STATE_DESC psoDesc;
+	ZeroMemory(&psoDesc, sizeof(D3D12_GRAPHICS_PIPELINE_STATE_DESC));
+	psoDesc.InputLayout = { mInputLayout.data(), (UINT)mInputLayout.size() };
+	psoDesc.pRootSignature = mRootSignature.Get();
+	psoDesc.VS =
+	{
+		reinterpret_cast<BYTE*>(mvsByteCode->GetBufferPointer()),
+		mvsByteCode->GetBufferSize()
 	};
-    psoDesc.PS = 
-	{ 
-		reinterpret_cast<BYTE*>(mpsByteCode->GetBufferPointer()), 
-		mpsByteCode->GetBufferSize() 
+	psoDesc.PS =
+	{
+		reinterpret_cast<BYTE*>(mpsByteCode->GetBufferPointer()),
+		mpsByteCode->GetBufferSize()
 	};
-    psoDesc.RasterizerState = CD3DX12_RASTERIZER_DESC(D3D12_DEFAULT);
+	psoDesc.RasterizerState = CD3DX12_RASTERIZER_DESC(D3D12_DEFAULT);
 	//psoDesc.RasterizerState.FillMode = D3D12_FILL_MODE_WIREFRAME;
 	psoDesc.RasterizerState.CullMode = D3D12_CULL_MODE_NONE;
-    psoDesc.BlendState = CD3DX12_BLEND_DESC(D3D12_DEFAULT);
-    psoDesc.DepthStencilState = CD3DX12_DEPTH_STENCIL_DESC(D3D12_DEFAULT);
-    psoDesc.SampleMask = UINT_MAX;
-    psoDesc.PrimitiveTopologyType = D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
-    psoDesc.NumRenderTargets = 1;
-    psoDesc.RTVFormats[0] = mBackBufferFormat;
-    psoDesc.SampleDesc.Count = m4xMsaaState ? 4 : 1;
-    psoDesc.SampleDesc.Quality = m4xMsaaState ? (m4xMsaaQuality - 1) : 0;
-    psoDesc.DSVFormat = mDepthStencilFormat;
-    ThrowIfFailed(md3dDevice->CreateGraphicsPipelineState(&psoDesc, IID_PPV_ARGS(&mPSO)));
+	psoDesc.BlendState = CD3DX12_BLEND_DESC(D3D12_DEFAULT);
+	psoDesc.DepthStencilState = CD3DX12_DEPTH_STENCIL_DESC(D3D12_DEFAULT);
+	psoDesc.SampleMask = UINT_MAX;
+	psoDesc.PrimitiveTopologyType = D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
+	psoDesc.NumRenderTargets = 1;
+	psoDesc.RTVFormats[0] = mBackBufferFormat;
+	psoDesc.SampleDesc.Count = m4xMsaaState ? 4 : 1;
+	psoDesc.SampleDesc.Quality = m4xMsaaState ? (m4xMsaaQuality - 1) : 0;
+	psoDesc.DSVFormat = mDepthStencilFormat;
+	ThrowIfFailed(md3dDevice->CreateGraphicsPipelineState(&psoDesc, IID_PPV_ARGS(&mPSO)));
 }
 
 void App::BuildModel()
@@ -316,18 +311,18 @@ void App::BuildModel()
 
 	//ThrowIfFailed(tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err, MODEL_PATH.c_str(), TEXTURE_PATH.c_str(), true, true));
 	tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err, MODEL_PATH.c_str());
-	for (const auto& shape : shapes) 
+	for (const auto& shape : shapes)
 	{
 		for (const auto& index : shape.mesh.indices)
 		{
 			Vertex vertex = {};
 
 			vertex.Pos = {
-				attrib.vertices[3* index.vertex_index + 0],
-				attrib.vertices[3* index.vertex_index + 1],
-				attrib.vertices[3* index.vertex_index + 2]
+				attrib.vertices[3 * index.vertex_index + 0],
+				attrib.vertices[3 * index.vertex_index + 1],
+				attrib.vertices[3 * index.vertex_index + 2]
 			};
-			
+
 			vertex.texCoord = {
 				attrib.texcoords[2 * index.texcoord_index + 0],
 				attrib.texcoords[2 * index.texcoord_index + 1]
@@ -339,7 +334,7 @@ void App::BuildModel()
 			indices.push_back(indices.size());
 		}
 	}
-	
+
 	const UINT vbByteSize = (UINT)vertices.size() * sizeof(Vertex);
 	const UINT ibByteSize = (UINT)indices.size() * sizeof(std::uint32_t);
 
--- ParticleManager.h
+++ ParticleManager.h
@@ -3,27 +3,6 @@
 #include "stdafx.h"
 #include "Particle.h"
 
-struct ParticleItems
-{
-	// Index into GPU constant buffer corresponding to the ObjectCB for this render item.
-	UINT ObjCBIndex = -1;
-
-	MeshGeometry* Geo = nullptr;
-
-	// Primitive topology.
-	D3D12_PRIMITIVE_TOPOLOGY PrimitiveType = D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST;
-
-	// DrawIndexedInstanced parameters.
-	UINT IndexCount = 0;
-	UINT StartIndexLocation = 0;
-	int BaseVertexLocation = 0;
-
-	const float height = 0.01f, width = 0.01f, velocity = 0.5f + (float)((rand() % 500) / 450);
-
-	//Particle Information
-	XMFLOAT3 position;
-	XMFLOAT3 colour;
-};
 
 class ParticleManager
 {
@@ -32,11 +11,7 @@ public:
 	~ParticleManager();
 
 	void Update(XMMATRIX&, float);
-<<<<<<< HEAD
-	void Render(Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> &commandList, ComPtr<ID3D12DescriptorHeap>&, UINT, Microsoft::WRL::ComPtr<ID3D12Device> &device, std::unique_ptr<MeshGeometry>& mesh);
-=======
-	void Render(Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> &commandList, ComPtr<ID3D12DescriptorHeap>&, UINT );
->>>>>>> parent of 5b3725a... Switch in render item
+	void Render(Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> &commandList, ComPtr<ID3D12DescriptorHeap>&, UINT, Microsoft::WRL::ComPtr<ID3D12Device> &device);
 	void UpdateCBuffers(std::unique_ptr<UploadBuffer<ObjectConstants>> &);
 	std::unique_ptr<MeshGeometry> mGeo = nullptr;
 	MeshGeometry GetMeshGeo() { return *mGeo; };
@@ -46,26 +21,8 @@ public:
 
 private:
 
-<<<<<<< HEAD
-	GeometryGenerator geoGen;
-	GeometryGenerator::MeshData meshs;
 	const int numberOfParticles = 30;
 
-	//D3D12 Variables
-	SubmeshGeometry gridSubmesh;
-
-	std::unique_ptr<MeshGeometry> meshGeo = nullptr;
-	UINT gridVertexOffset;
-	UINT gridIndexOffset;
-	GeometryGenerator::MeshData meshing;
-
-	const int numberOfParticles = 1;
-
-=======
-	const int numberOfParticles = 1;
->>>>>>> parent of 8d743fa... Multiple Particles
-	
 	//Particle* mParticle[];
-	std::vector<std::unique_ptr<ParticleItems>> mParticles;
+	std::vector<Particle*> mParticles;
 };
-
